8cb6f7cfb6f74d4a8522f5d79cbad0f362da1f7a
Nattanon Kh <weisetechinfo@gmail.com>
2024-09-25 17:27:44 +0700

    add trust proxy v0.2

--- db-orm/index.ts
+++ db-orm/index.ts
@@ -7,8 +7,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 app.enable('trust proxy');
+app.set('trust proxy', true);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 app.get('/priceCarsBrand/:group', async (req,res) => {
     const { group } = req.params;
