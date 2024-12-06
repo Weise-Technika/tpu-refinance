69535c30a4539a48760298ce4af532326ccac26d
Nattanon Kh <weisetechinfo@gmail.com>
2024-12-06 23:47:46 +0700

    update mjs

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,32 +1,56 @@
+// import { defineConfig } from 'astro/config';
+// import vue from '@astrojs/vue';
+
+// import cloudflare from '@astrojs/cloudflare';
+// import dotenv from 'dotenv';
+// import clearConsole from 'vite-plugin-clear-console';
+
+// dotenv.config();
+
+// export default defineConfig({
+//   integrations: [vue()],
+
+//   vite: {
+//     logLevel: 'silent',
+//     server: {
+//       plugins: [clearConsole()],
+//       watch: {
+//         usePolling: true
+//       },
+//     },
+//     envPrefix: 'PUBLIC_',
+//     define: {
+//       'process.env': process.env
+//     }
+//   },
+//   output: 'server',
+//   adapter: cloudflare({
+//     certificate: 'cert/certificate.pem',
+//     key: 'cert/private-key.pem'
+//   })
+// });
+
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
 import cloudflare from '@astrojs/cloudflare';
-import dotenv from 'dotenv';
-import clearConsole from 'vite-plugin-clear-console';
-
-dotenv.config();
 
 export default defineConfig({
   integrations: [vue()],
 
   vite: {
-    logLevel: 'silent',
     server: {
-      plugins: [clearConsole()],
       watch: {
         usePolling: true
       },
     },
-    envPrefix: 'PUBLIC_',
-    define: {
-      'process.env': process.env
-    }
   },
+
   output: 'server',
   adapter: cloudflare({
+    // Add your Cloudflare configuration here
+    // For example, if you need to specify a certificate:
     certificate: 'cert/certificate.pem',
     key: 'cert/private-key.pem'
   })
-  
 });
